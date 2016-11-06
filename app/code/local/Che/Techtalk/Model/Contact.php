5f328fd20515741c8f9c35580a7afa20fbae8737
Olha <cherepovskaya@gmail.com>
2016-11-06 23:23:27 +0200

    task#3 bug getRequestRecord

---
+++ app/code/local/Che/Techtalk/Model/Contact.php
@@ -0,0 +1,12 @@
+<?php
+
+class Che_Techtalk_Model_Contact extends Mage_Core_Model_Abstract
+{
+    /**
+     * Initialize resource model
+     */
+    protected function _construct()
+    {
+        $this->_init('techtalk/contact');
+    }
+}
