28314db8355545312e1e1c469129b4a72f9695c8
Olha <cherepovskaya@gmail.com>
2016-11-17 12:21:22 +0200

    #task3 +getCollection

--- app/code/local/Che/Techtalk/Block/View.php
+++ app/code/local/Che/Techtalk/Block/View.php
@@ -6,6 +6,9 @@ class Che_Techtalk_Block_View extends Mage_Core_Block_Template
     {
         return Mage::getModel('techtalk/contact')->load(1);
     }
-
+    public function getCollection()
+    {
+        return Mage::getModel('techtalk/contact')->getCollection();
+    }
 
 }
